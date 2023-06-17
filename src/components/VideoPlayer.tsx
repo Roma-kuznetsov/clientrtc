827861a5afa121d847744796120dc67efef5767b
Roman Kuznetsov <romakuznetsov832@gmail.com>
2023-06-17 13:43:47 +0300

    RTC voice and video

---
+++ src/components/VideoPlayer.tsx
@@ -0,0 +1,11 @@
+import { FC, useEffect, useRef } from "react";
+
+export const VideoPlayer: FC<{ stream: MediaStream }> = ({ stream }) => {
+  const videoRef = useRef<HTMLVideoElement>(null)
+  useEffect(() => {
+    if (videoRef.current) videoRef.current.srcObject = stream;
+  }, [stream])
+  return (
+    <video ref={videoRef} autoPlay muted={false} />
+  )
+}
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,16 +1,23 @@
 import { FC, useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { RoomContext } from "../context/RoomContect";
+import { VideoPlayer } from "../components/VideoPlayer";
+import { PeerState } from "../context/peerReducer";
 
 export const Room: FC = () => {
   const { id } = useParams()
-  const { ws } = useContext(RoomContext)
+  const { ws, me, stream, peers } = useContext(RoomContext)
   useEffect(() => {
-    ws.emit("join-room", { roomId: id })
-  }, [id])
+    console.log(me)
+    if (me) ws.emit("join-room", { roomId: id, peerId: me._id })
+  }, [id, me, ws, stream])
   return (
-    <div>
-      room{id}
-    </div>
+    <>
+      <h1>roomID : {id}</h1>
+
+      {Object.values(peers as PeerState).map((peer) => (
+        <div><VideoPlayer stream={peer.stream} /></div>
+      ))}
+    </>
   );
 };
